b6b5db5b4723456afb439bc3cafe07611bb0b425
Trọng <haquangtrong.ton@gmail.com>
2020-05-28 19:39:45 +0700

    Update test sign in

---
+++ Test Suites/1612745 - Run all test cases.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>1612745 - Run all test cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>81228898-9bfa-4794-9dee-d031ae8d653c</testSuiteGuid>
+   <testCaseLink>
+      <guid>556b97b1-d69b-4116-ac2f-285ca9dd23d7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1612745 - Test sign in</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>d7b43ca8-16c6-4757-9e27-c7873a7d9e05</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/1612745 - Test data sign in</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>d7b43ca8-16c6-4757-9e27-c7873a7d9e05</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>9745f65b-59d3-4117-93cf-a0a2a5e35dd2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>d7b43ca8-16c6-4757-9e27-c7873a7d9e05</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>406a237d-3de8-4ced-a9cc-75c3564c1180</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
