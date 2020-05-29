42e747911e3c6a93a5ab66751d683deaed8af42f
t2ntruong811 <t2ntruong811@gmail.com>
2020-05-29 17:06:29 +0700

    Test case Edit Email use CSV
    Test case Account Settings Edit Email CSV

---
+++ Test Suites/1612759 - Run all test cases.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>1612759 - Run all test cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e0813763-a2de-4398-9a9d-e29ac61bcf2c</testSuiteGuid>
+   <testCaseLink>
+      <guid>29b1bc9a-66ba-436e-acb9-ac4b4dc04876</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1612759 - Test case use CSV/1612759 - Test Account Settings - Edit Email</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>e5e6e245-a6db-4a24-a7e9-552620e3e319</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/1612759 - Test Data CSV/1612759 - Data CSV Test Account Settings - Edit Email</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>e5e6e245-a6db-4a24-a7e9-552620e3e319</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>﻿newEmail</value>
+         <variableId>35b6865f-1ade-4792-80bc-618a372408a5</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>e5e6e245-a6db-4a24-a7e9-552620e3e319</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>confirmEmail</value>
+         <variableId>936c060c-e1b7-4816-b950-411bf06415d2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>e5e6e245-a6db-4a24-a7e9-552620e3e319</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>90bac033-b28f-49e3-bc05-98c19c4cde2e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
